5db1f0ea9ba5091f52983d7ddbd89cce4a453215
Novoa Champion <novoa_champion@mac.com>
2023-09-08 20:21:50 -0400

    Initial Commit

---
+++ OSTU/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OSTU
+//
+//  Created by Novoa Champion on 9/8/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
