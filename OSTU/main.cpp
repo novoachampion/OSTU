fb0cb06395429afa2864f1b64648140ad267c963
Novoa Champion <novoa_champion@mac.com>
2023-09-08 20:54:47 -0400

    fixed bug a

--- OSTU/main.cpp
+++ OSTU/main.cpp
@@ -9,6 +9,6 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World Updated!\n";
+    std::cout << "Hello, World Updated bug a!\n";
     return 0;
 }
